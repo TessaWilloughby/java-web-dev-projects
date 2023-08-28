c1d8dd95ca64be4fb19801bf4098043c9940ec4a
Tessa Willoughby <tessa.willoughby@me.com>
2023-08-28 17:26:01 -0400

    added four additional classes

---
+++ classes-part-one/exercises/src/main/java/Teacher.java
@@ -0,0 +1,49 @@
+public class Teacher {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String subject;
+
+    private String yearsTeaching;
+
+    public Teacher(String firstName, String lastName, String subject, String yearsTeaching) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.subject = subject;
+        this.yearsTeaching = yearsTeaching;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setYearsTeaching(String yearsTeaching) {
+        this.yearsTeaching = yearsTeaching;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getYearsTeaching() {
+        return yearsTeaching;
+    }
+}
